ecdb01ce7ff4fb4ee544359ab7e6db2f1453cd65
fkumsuz <132204486+fkumsuz@users.noreply.github.com>
2024-04-25 19:04:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
-
-@app.route('/')
+ 
+image_folder = "images/image.jpg"
+ 
+@app.route('/', methods=['GET'])
 def index():
-    # Route to serve the HTML page
-    return render_template('index.html')
-
-@app.route('docs/assets/<path:filename>')
-def assets(filename):
-    # Route to serve the image file
-    return send_from_directory('docs/assets/', filename)
+    return render_template('index.html', image_path=image_folder )
 
 if __name__ == '__main__':
     app.run(debug=True)
